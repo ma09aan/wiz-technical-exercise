50d22c2991361e34078b4ec78f37d96145e65a28
Thilina Viraj <thilinaviraj950@gmail.com>
2025-06-01 15:07:08 +0530

    state file created

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "your-unique-terraform-state-bucket-name" # REPLACE THIS
-#     key            = "wiz-exercise/global/s3/terraform.tfstate"
-#     region         = "us-east-1"                               # REPLACE WITH YOUR CHOSEN REGION
-#     encrypt        = true
-#     dynamodb_table = "your-unique-terraform-state-lock-table"  # REPLACE THIS
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "s3-for-state-wiz" # REPLACE THIS
+    key            = "wiz-exercise/global/s3/terraform.tfstate"
+    region         = "us-east-1"                               # REPLACE WITH YOUR CHOSEN REGION
+    encrypt        = true
+    dynamodb_table = "s3-for-state-wiz"  # REPLACE THIS
+  }
+}
 
